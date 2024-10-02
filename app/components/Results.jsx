052c053ee4016250f0f0e759efe49e6da920d508
Eriola <endoj22@gmail.com>
2024-10-02 07:49:38 +0200

    withSearchParams

--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { battle } from '../utils/api';
 import PropTypes from "prop-types";
 import Loading from './Loading';
+import withSearchParams from './withSearchParams';
+import { Link } from 'react-router-dom';
 
 function Card({ profile }) {
   const {
@@ -64,80 +66,87 @@ Card.propTypes = {
   }).isRequired,
 };
 
-export default class Results extends React.Component {
-    constructor(props) {
-        super(props)
+class Results extends React.Component {
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            winner: null,
-            loser: null,
-            error: null,
-            loading: true
-        }
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    const sp = this.props.router.searchParams;
+    const playerOne = sp.get("playerOne");
+    const playerTwo = sp.get("playerTwo");
+
+    battle([playerOne, playerTwo]).then((players) => {
+      this.setState({
+        winner: players[0],
+        loser: players[1],
+        error: null,
+        loading: false
+      })
+    }).catch(({ message }) => {
+      this.setState({
+        error: message,
+        loading: false
+      })
+    })
+  }
+
+  render() {
+    const { winner, loser, error, loading } = this.state;
+
+    if (loading === true) {
+      return <Loading text="Battling " />
     }
 
-    componentDidMount() {
-        const {playerOne,playerTwo} = this.props;
-
-        battle([playerOne,playerTwo]).then((players) => {
-            this.setState({
-                winner: players[0],
-                loser: players[1],
-                error: null,
-                loading: false
-            })
-        }).catch(({message}) => {
-            this.setState({
-                error: message,
-                loading: false
-            })
-        })
+    if (error) {
+      return <p className='text-center error'>{error}</p>
     }
 
-    render(){
-        const {winner,loser, error, loading} = this.state;
-
-        if(loading === true){
-            return <Loading text="Battling "/>
-        }
-
-        if(error) {
-            return <p className='text-center error'>{error}</p>
-        }
-
-        return (
-           <main className='animate-in stack main-stack'>
-            <div className='split'>
-                <h1>Results</h1>
-            </div>
-            <section className='grid'>
-                <article className='results-container'>
-                    <Card profile={winner.profile}/>
-                    <p className='results'>
-                        <span>
-                            {winner.score === loser.score ? 'Tie ' : 'Winner '}
-                            {winner.score.toLocaleString()}
-                        </span>
-                        {winner.score !== loser.score && (
-                            <img
-                            width={80}
-                            src='https://ui.dev/images/certificate.svg'
-                            alt='Certificate'
-                            />
-                        )}
-                    </p>
-                </article>
-                <article className='results-container'>
-                    <Card profile={loser.profile}/>
-                    <p className='results'>
-                        <span>
-                            {winner.score === loser.score ? 'Tie ' : 'Winner '}
-                            {loser.score.toLocaleString()}
-                        </span>
-                    </p>
-                </article>
-            </section>
-           </main>
-        )
-    }
+    return (
+      <main className='animate-in stack main-stack'>
+        <div className='split'>
+          <h1>Results</h1>
+        </div>
+        <Link to='/battle' className='btn secondary'>
+          Reset
+        </Link>
+        <section className='grid'>
+          <article className='results-container'>
+            <Card profile={winner.profile} />
+            <p className='results'>
+              <span>
+                {winner.score === loser.score ? 'Tie' : 'Winner'}{" "}
+                {winner.score.toLocaleString()}
+              </span>
+              {winner.score !== loser.score && (
+                <img
+                  width={80}
+                  src='https://ui.dev/images/certificate.svg'
+                  alt='Certificate'
+                />
+              )}
+            </p>
+          </article>
+          <article className='results-container'>
+            <Card profile={loser.profile} />
+            <p className='results'>
+              <span>
+                {winner.score === loser.score ? 'Tie' : 'Loser'}{" "}
+                {loser.score.toLocaleString()}
+              </span>
+            </p>
+          </article>
+        </section>
+      </main>
+    )
+  }
 }
+
+export default withSearchParams(Results);
--- app/index.jsx
+++ app/index.jsx
@@ -4,18 +4,37 @@ import './index.css';
 import Popular from './components/Popular';
 import Battle from './components/Battle';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Results from './components/Results';
 
 class App extends React.Component {
+    constructor(prop) {
+        super(prop);
+
+        this.state = {
+            theme: "light",
+        };
+
+        this.toggleTheme = this.toggleTheme.bind(this);
+    }
+
+    toggleTheme() {
+        this.setState(({ theme }) => ({
+            theme: theme === "light" ? "dark" : "light",
+        }));
+    }
+
     render() {
         return (
             <Router>
-                <div className='dark'>
+                <div className={this.state.theme}>
                     <div className='container'>
+                        <Nav theme={this.state.theme} toggleTheme={this.toggleTheme} />
                         <Routes>
-                            <Route path='/' element={<Popular/>} />
-                            <Route path='/battle' element={<Battle/>} />
+                            <Route path='/' element={<Popular />} />
+                            <Route path='/battle' element={<Battle />} />
+                            <Route path='/results' element={<Results />} />
                         </Routes>
-                        <Popular />
                     </div>
                 </div>
             </Router>
